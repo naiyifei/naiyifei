d3c3dffa01148eec80a5c4ca684f8e63ee8f7251
gxsky <gxsky@naiyifeidemac-mini.local>
2016-10-26 15:44:26 +0800

    96

--- lanuchimage/AppDelegate.m
+++ lanuchimage/AppDelegate.m
@@ -25,7 +25,7 @@
     self.window =  [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
  
-    //ssdsdsd
+    //你好
     
     xxViewController *xx = [[xxViewController alloc] init];
     
